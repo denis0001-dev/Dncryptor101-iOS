b743498582f37d53bfa2c6d86480354ba2bb353b
denis0001-dev <denis0001.dev@ya.ru>
2025-05-07 23:50:05 +0300

    Initial Commit

---
+++ Dncryptor101/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dncryptor101
+//
+//  Created by denis0001-dev on 07.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dncryptor101/Dncryptor101App.swift
@@ -0,0 +1,17 @@
+//
+//  Dncryptor101App.swift
+//  Dncryptor101
+//
+//  Created by denis0001-dev on 07.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Dncryptor101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
