e01bc0564651299209a6d42c78fb1ab8bcad5744
denis0001-dev <denis0001.dev@ya.ru>
2025-05-08 01:33:48 +0300

    Add "About app" menu

---
+++ Dncryptor101/AppIcon.swift
@@ -0,0 +1,29 @@
+//
+//  AppIcon.swift
+//  Dncryptor101
+//
+//  Created by denis0001-dev on 08.05.2025.
+//
+
+import Foundation
+import SwiftUI
+
+struct AppIcon: View {
+    var body: some View {
+        Bundle.main.iconFileName
+            .flatMap { UIImage(named: $0) }
+            .map { Image(uiImage: $0) }
+    }
+}
+
+extension Bundle {
+    var iconFileName: String? {
+        guard let icons = infoDictionary?["CFBundleIcons"] as? [String: Any],
+              let primaryIcon = icons["CFBundlePrimaryIcon"] as? [String: Any],
+              let iconFiles = primaryIcon["CFBundleIconFiles"] as? [String],
+              let iconFileName = iconFiles.last else {
+            return nil
+        }
+        return iconFileName
+    }
+}
--- Dncryptor101/ContentView.swift
+++ Dncryptor101/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Text("A List Item")
+                Text("A Second List Item")
+                Text("A Third List Item")
+            }
+            .navigationTitle("Dncryptor101")
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    NavigationLink {
+                        DetailView()
+                    } label: {
+                        Image(systemName: "info.circle")
+                            .font(.headline)
+                            .foregroundColor(.accentColor)
+                    }
+                }
+            }
         }
-        .padding()
+        VStack {
+            
+        }
+    }
+}
+
+struct DetailView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                AppIcon()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                Text("Dncryptor101")
+                    .font(.title)
+                    .bold()
+                    .padding(.bottom, 5)
+                Text("An app to help you encrypt & decrypt your text!")
+                Spacer()
+            }
+            .padding()
+        }
+        .navigationTitle("About app")
     }
 }
 
